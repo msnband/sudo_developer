7fb69b930474d9ecaf9a32ae2fb2ca9ea568605a
Mohammad Hossein Abdsharifi <msnband@hotmail.com>
2020-06-02 23:37:16 +0200

    Defining variables in user class

--- class_file.py
+++ class_file.py
@@ -7,3 +7,18 @@ import time
 import pickle
 
 class User:
+
+    def __init__(self, user_name, password, privileges):
+        """ Defining variables in user class"""
+        self.user_name = user_name
+        self.password = password
+        self.privileges = privileges
+        self.currentPath = ""
+        self.index = 0
+        self.file_name = ""
+
+
+
+
+    def readfile(self, file_name):
+        os.chdir(self.currentPath)
