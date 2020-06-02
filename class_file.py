eadd9f038ae40c8bc6e706afe9df16347015f6f1
Mohammad Hossein Abdsharifi <msnband@hotmail.com>
2020-06-03 00:10:50 +0200

    trying to read the file!

--- class_file.py
+++ class_file.py
@@ -17,8 +17,43 @@ class User:
         self.index = 0
         self.file_name = ""
 
+    
+
+
+
+    def read_noninput(self):
+        temporary = self.file_name
+        self.file_name = ""
+        self.index = 0
+        message = "file has been closed"
+        return temporary + message
 
 
 
     def readfile(self, file_name):
+        """ Change to current user directory """
         os.chdir(self.currentPath)
+
+        """ opening the file for reading """
+
+        try:
+
+            if self.file_name == file_name:
+                file_to_read = open(self.file_name, 'r')
+                outcome = file_to_read.read(self.index + 100)
+                outcome = outcome[self.index: ]
+                if len(outcome) < 100:
+                    self.index = -100
+                file_to_read.close()
+                self.index = self.index + 100
+
+            else:
+                self.file_name = file_name
+                file_to_read = open(self.file_name, 'r')
+                outcome = file_to_read.read(100)
+                file_to_read.close()
+                self.index = 100
+            
+            return "File content are: \n\n" + outcome + "\n\n"
+
+
