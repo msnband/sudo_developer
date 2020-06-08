bb5b3289dbcf2871873dc672bfe8b755f8c555d3
Mohammad Hossein Abdsharifi <msnband@hotmail.com>
2020-06-08 18:04:33 +0200

    file test first function (not working)

---
+++ file_test1.py
@@ -0,0 +1,32 @@
+
+import os
+import server1
+import class_file
+import pickle
+import unittest
+
+main_path = os.getcwd()
+server1.my_server()
+
+class TCPconnectionCheck(unittest.TestCase):
+
+    def login_register_test(self):
+
+        os.chdir(main_path)
+        serv = server1.my_server()
+        expected_outcome = ["Congratulations. New user is registered succesfully...", "Username you enterd is not valid or already exists"]
+        outcome = []
+
+        outcome.append(serv.register('temp', 'pwd', 'user')[:27])
+        outcome.append(serv.register('temp', 'pwd', 'user')[:27])
+
+        expected_login_outcome = "temp: You logged in successfully."
+        login_outcome = (serv.login('temp', 'pwd', 'tcp port')[:22])
+
+        with open ('reg.pickle', 'rb') as listFile:
+            user_list = pickle.load(listFile)
+
+        try:
+            os.removedirs(os.path.join('Users', 'temp'))
+        except FileNotFoundError:
+            os.removedirs(os.path.join("Admins", "temp"))
